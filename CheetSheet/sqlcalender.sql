16de66add2764091b2a49e9bbfa4a59d1c67771a
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-12-02 21:51:33 -0500

    Challenge 2 - SQL Calender

--- CheetSheet/sqlcalender.sql
+++ CheetSheet/sqlcalender.sql
@@ -18,3 +18,18 @@ FROM rentals
 GROUP BY customer_name
 HAVING COUNT(DISTINCT activity) > 1;
 
+2. Santa wants to know which gifts weigh more than 1 kg. Can you list them?
+
+Table name: gifts
+
+gift_name	recipient	weight_kg
+Toy Train	John	2.5
+Chocolate Box	Alice	0.8
+Teddy Bear	Sophia	1.2
+Board Game	Liam	0.9
+
+Solution:
+SELECT gift_name 
+FROM gifts 
+WHERE weight_kg > 1;
+
