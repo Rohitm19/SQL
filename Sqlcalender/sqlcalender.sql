34ecaef80ddbd09674ef3b46f0539e0ad53729d6
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-12-03 23:25:24 -0500

    Challenge 3 - SQL Calender Question

--- Sqlcalender/sqlcalender.sql
+++ Sqlcalender/sqlcalender.sql
@@ -33,3 +33,17 @@ SELECT gift_name
 FROM gifts 
 WHERE weight_kg > 1;
 
+3. You’re trying to identify the most calorie-packed candies to avoid during your holiday binge. Write a query to rank candies based on their calorie count within each category. Include the candy name, category, calories, and rank (rank_in_category) within the category.
+
+Table name: candy_nutrition
+
+candy_id	candy_name	calories	candy_category
+1	Candy Cane	200	Sweets
+2	Chocolate Bar	250	Chocolate
+3	Gingerbread Cookie	150	Baked Goods
+4	Lollipop	100	Sweets
+5	Dark Chocolate Truffle	180	Chocolate
+6	Marshmallow	900	Sweets
+7	Sugar Cookie	140	Baked Goods
+
+
