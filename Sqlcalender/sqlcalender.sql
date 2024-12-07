82563674a7714b9797aa16f2e57b350695e47e82
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-12-06 20:11:39 -0500

    Challenge 4 - SQL Calender

--- Sqlcalender/sqlcalender.sql
+++ Sqlcalender/sqlcalender.sql
@@ -55,3 +55,34 @@ SELECT
 FROM 
     candy_nutrition;
 
+4. You’re planning your next ski vacation and want to find the best regions with heavy snowfall. Given the tables resorts and snowfall, find the average snowfall for each region and sort the regions in descending order of average snowfall. Return the columns region and average_snowfall.
+
+Table name: ski_resorts
+
+resort_id	resort_name	region
+1	Snowy Peaks	Rocky Mountains
+2	Winter Wonderland	Wasatch Range
+3	Frozen Slopes	Alaska Range
+4	Powder Paradise	Rocky Mountains
+Table name: snowfall
+
+resort_id	snowfall_inches
+1	60
+2	45
+3	75
+4	55
+
+Solution:
+SELECT 
+    sr.region, 
+    ROUND(AVG(sf.snowfall_inches), 2) AS average_snowfall
+FROM 
+    ski_resorts sr
+JOIN 
+    snowfall sf
+ON 
+    sr.resort_id = sf.resort_id
+GROUP BY 
+    sr.region
+ORDER BY 
+    average_snowfall DESC;
