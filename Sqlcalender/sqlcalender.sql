498cc704d325bac75b984e473ca31385e8b83ec4
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-12-06 20:21:57 -0500

    Challenge 5

--- Sqlcalender/sqlcalender.sql
+++ Sqlcalender/sqlcalender.sql
@@ -86,3 +86,24 @@ GROUP BY
     sr.region
 ORDER BY 
     average_snowfall DESC;
+
+5. This year, we are celebrating Christmas in the Southern Hemisphere! Which beaches are expected to have temperatures above 30°C on Christmas Day?
+
+Table name: beach_temperature_predictions
+
+beach_name	country	expected_temperature_c	date
+Bondi Beach	Australia	32	2024-12-24
+Copacabana Beach	Brazil	28	2024-12-24
+Clifton Beach	South Africa	31	2024-12-25
+Brighton Beach	New Zealand	25	2024-12-25
+
+Solution:
+SELECT 
+    beach_name, 
+    country, 
+    expected_temperature_c 
+FROM 
+    beach_temperature_predictions
+WHERE 
+    date = '2024-12-25' AND 
+    expected_temperature_c > 30;
